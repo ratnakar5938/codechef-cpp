a86c3f564daad3bdded80d5fa3bfa73ab9c971c1
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-04 22:28:13 +0530

    triangle with angle

---
+++ triangle_with_angle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (((a + b + c) == 180) && a > 0 && b > 0 && c > 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
