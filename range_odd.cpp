0fe1b3d2d9d5fa97e1d6c9f5afc240fa8f0b1b89
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-04 20:34:38 +0530

    Range odd

---
+++ range_odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int l, r;
+    cin >> l >> r;
+    for (int i = l; i <= r; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
