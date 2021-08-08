8462f265d0906a26ba175be696ed01b453d3ea4e
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-08 20:58:14 +0530

    Valid pair

---
+++ valid_pair.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a == b || a == c)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
