e45956120c0aeed01c0e4e0077d56b8f0c06087c
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-16 00:18:24 +0530

    chefland visa

---
+++ chefland_visa.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    int x1, x2, y1, y2, z1, z2;
+    while (t--)
+    {
+        cin >> x1 >> x2 >> y1 >> y2 >> z1 >> z2;
+        if (x2 >= x1 && y2 >= y1 && z2 <= z1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
